995f50daa5d7588b971bb096d1afeed406c6cf73
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-30 18:11:52 +0200

    ampoule.js

---
+++ javascript/ampoule.js
@@ -0,0 +1,16 @@
+// Fonction pour allumer ou éteindre l'ampoule
+function OnOffAmpoule() {
+    var ampoule = document.getElementById("ampoule");
+
+    // Obtenez le nom de l'image actuelle
+    var currentSrc = ampoule.getAttribute("src");
+
+    // Vérifiez si l'ampoule est allumée ou éteinte
+    if (currentSrc.search("pic_bulboff.gif") !== -1) {
+        // Si elle est éteinte, allumez-la
+        ampoule.setAttribute("src", "pic_bulbon.gif");
+    } else {
+        // Sinon, éteignez-la
+        ampoule.setAttribute("src", "pic_bulboff.gif");
+    }
+}
