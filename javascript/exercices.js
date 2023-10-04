403fd37888951862ba0a689908d77ae32d55b1e7
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-10-04 09:51:16 +0200

    exercice js

--- javascript/exercices.js
+++ javascript/exercices.js
@@ -57,49 +57,46 @@ function Fibonacci(n)
 
 
 // Fonction Appeler_Range :
-//		- appelée par un événement javascript depuis une page html (comme "onclick")
-//      - lit les données dans les entrées html
-//		- appelle la fonction Range()
 function Appeler_Range()
 {
 	// lecture des valeurs saisies qui sont converties en nombre
 	const stop = Number(document.forms["range"]["stop"].value);
 	const start = Number(document.forms["range"]["start"].value);
-	const step = Number(document.forms["range"]["step"].value);
+	let step = Number(document.forms["range"]["step"].value);
+
+	if (step == 0)
+	{
+		step = 1;
+	}
 	
 	// appel de la fonction Range
 	let range = Range(stop, start, step);
 	
 	// affichage du résultat dans la page html
-	let resultat = document.getElementById("resultat_range").innerHTML = "Résultat : Range(" + stop + ", " + start + ", " + step + ") = " + range;
+	rangeHTML = "[";
+	for(let i=0; i<range.length; i++)
+	{
+		rangeHTML += range[i];
+		if (i<range.length-1)
+		{
+			rangeHTML += ", ";
+		}
+	}
+	rangeHTML += "]";
+	let resultat = document.getElementById("resultat_range").innerHTML = "Résultat : Range(" + stop + ", " + start + ", " + step + ") = " + rangeHTML;
 }
-
-
-// Fonction Range(stop, start=0, step=1)
-// Retourne/Renvoie le tableau des entiers compris entre start (inclus) et 
-// stop (exclu) avec un pas de step.
-// si start n'est pas défini, il vaut 0
-// si step n'est pas défini, il vaut 1
-// par simplification, on ne traitera que le cas où step est strictement positif
-// si stop est inférieur ou égal à start, on renvoie un tableau vide
-// exemple Range(10, 0) == range(10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// exemple Range(10, 4, 2) == [4, 6, 8]
-// exemple Range(-2, 0) == []
 
 function Range(stop, start = 0, step = 1) {
 	// Vérifier si stop est inférieur ou égal à start
-	if (stop <= start || step <= 0) {
-	  return [];
+	if (step <= 0) {
+		return undefined;
+	} else {
+		resultat = [];
+		for (let i = start; i < stop; i += step) {
+			resultat.push(i);
+		}
+		return resultat;
 	}
 
-	// Initialiser un tableau pour stocker les entiers
-	const result = [];
-  
-	// Boucle pour générer les entiers dans la plage spécifiée
-	for (let i = start; i < stop; i += step) {
-	  result.push(i);
-	}
-  
-	return result;
   }
 
